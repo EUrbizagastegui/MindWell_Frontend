8a44a2df7b3821450fb9fba6b7ccdbf0c4dfa5b8
EUrbizagastegui <erickurbizagastegui@hotmail.com>
2024-06-23 18:51:25 -0500

    Creado el login de la app

--- src/pages/signIn/SignIn.jsx
+++ src/pages/signIn/SignIn.jsx
@@ -1,13 +1,50 @@
 import './SignIn.css'
+import { useState, useRef } from 'react';
 import { InputText } from 'primereact/inputtext';
 import { Button } from 'primereact/button';
+import { Toast } from 'primereact/toast';
 import { useNavigate } from 'react-router-dom';
+import APIGatewayService from '../../shared/services/api-gateway-service';
 
 const SignIn = () => {
     const navegar = useNavigate()
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const toast = useRef(null);
+
+    const showToast = (severity, summary, detail) => {
+        toast.current.show({severity: severity, summary: summary, detail: detail});
+    }
+
+    const signIn = async () => {
+        if (email === '' || password === '') {
+            alert('Por favor, complete todos los campos.');
+            return;
+        }
+
+        const data = {
+            "email": email,
+            "password": password
+        }
+
+        try {
+            const response = await APIGatewayService.verifyPatient(data)
+            localStorage.setItem('user', JSON.stringify(response.data))
+            navegar('/home')
+        } catch (error) {
+            showToast('error', 'Error', 'Credenciales incorrectas')
+        }
+    }
+
+    const handleEnter = (event) => {
+        if (event.key === 'Enter') {
+            signIn()
+        }
+    }
 
     return (
         <div className="sign-in">
+            <Toast ref={toast} />
             <div className='si-left'>
                 <div className='sill'>
                     <h1>Bienvenido a MindWell!</h1>
@@ -17,11 +54,24 @@ const SignIn = () => {
             <div className="si-right">
                 <div className='sir-body'>
                     <label htmlFor="email">Email</label>
-                    <InputText id='email' placeholder='Email' />
+                    <InputText
+                    id='email'
+                    placeholder='Email'
+                    value={email}
+                    onChange={e => setEmail(e.target.value)}
+                    />
                     <label htmlFor="password">Contraseña</label>
-                    <InputText id='password' placeholder='Password' />
-                    <Button label='Iniciar sesión' onClick={() => navegar('/home')}/>
-
+                    <InputText
+                    id='password'
+                    placeholder='Password'
+                    value={password}
+                    onChange={e => setPassword(e.target.value)}
+                    />
+                    <Button
+                    label='Iniciar sesión'
+                    onClick={signIn}
+                    onKeyDown={handleEnter}
+                    />
                     <p>¿No tienes una cuenta? <a href='/sign-up'>Registrate</a></p>
                 </div>
             </div>
--- src/pages/signup/SignUp.jsx
+++ src/pages/signup/SignUp.jsx
@@ -1,26 +1,106 @@
 import './SignUp.css'
+import APIGatewayService from '../../shared/services/api-gateway-service';
+import { useState, useRef } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { Button } from 'primereact/button';
 import { InputText } from 'primereact/inputtext';
+import { Toast } from 'primereact/toast';
 
 const SignUp = () => {
+    const toast = useRef(null);
+    const navigate = useNavigate();
+
+    const [nombre, setNombre] = useState('')
+    const [edad, setEdad] = useState(0)
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [repetirPassword, setRepetirPassword] = useState('')
+
+    const showToast = (severity, summary, detail) => {
+        toast.current.show({severity: severity, summary: summary, detail: detail});
+    }
+
+    const register = async () => {
+        if (nombre === '' || edad === 0 || email === '' || password === '' || repetirPassword === '') {
+            showToast('error', 'Error', 'Por favor, complete todos los campos.')
+            return;
+        } else if (password !== repetirPassword) {
+            showToast('error', 'Error', 'Las contraseñas no coinciden.')
+            return;
+        }
+
+        const data = {
+            "name": nombre,
+            "age": edad,
+            "email": email,
+            "password": password
+        }
+
+        try {
+            const response = await APIGatewayService.createPatient(data)
+            showToast('success', 'Cuenta creada', 'Ahora inicia sesión.')
+            navigate('/sign-in')
+        } catch (error) {
+            showToast('error', 'Error', 'Ocurrió un error al crear el paciente. Por favor, intenta nuevamente.')
+            console.log(error)
+        }
+    }
+
+    const handleEnter = (event) => {
+        if (event.key === 'Enter') {
+            register()
+        }
+    }
+
     return (
         <div className="sign-up">
+            <Toast ref={toast} />
             <div className='su-body'>
                 <div className="sub-title">
                     <h1>Crea tu cuenta</h1>
                 </div>
                 <p>¿Ya tienes una cuenta? <a href='/sign-in'>Inicia sesión</a></p>
                 <label htmlFor="nombre">Nombre</label>
-                <InputText id='nombre' placeholder='Nombre' />
+                <InputText
+                id='nombre'
+                placeholder='Nombre'
+                value={nombre}
+                onChange={e => setNombre(e.target.value)}
+                />
                 <label htmlFor="edad">Edad</label>
-                <InputText id='edad' placeholder='Edad' />
+                <InputText
+                id='edad'
+                placeholder='Edad'
+                keyfilter="pnum"
+                value={edad}
+                onChange={e => setEdad(e.target.value)}
+                />
                 <label htmlFor="email">Email</label>
-                <InputText id='email' placeholder='Email' />
+                <InputText
+                id='email'
+                placeholder='Email'
+                value={email}
+                onChange={e => setEmail(e.target.value)}
+                />
                 <label htmlFor="contraseña">Contraseña</label>
-                <InputText id='contraseña' placeholder='Contraseña' />
+                <InputText
+                id='contraseña'
+                placeholder='Contraseña'
+                value={password}
+                onChange={e => setPassword(e.target.value)}
+                />
                 <label htmlFor="repetir-contraseña">Repetir contraseña</label>
-                <InputText id='repetir-contraseña' placeholder='Repetir contraseña' />
-                <Button label='Registrarse' />
+                <InputText
+                id='repetir-contraseña'
+                placeholder='Repetir contraseña'
+                value={repetirPassword}
+                onChange={e => setRepetirPassword(e.target.value)}
+                />
+                <Button
+                label='Registrarse'
+                onClick={register}
+                onKeyDown={handleEnter}
+                />
             </div>
         </div>
     )

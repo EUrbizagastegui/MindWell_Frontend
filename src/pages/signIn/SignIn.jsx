ab482c8e7a534cfbad6321a165a38c580b70af0a
EUrbizagastegui <erickurbizagastegui@hotmail.com>
2024-06-22 23:55:03 -0500

    Creadas las plantillas de login.

--- src/pages/signIn/SignIn.jsx
+++ src/pages/signIn/SignIn.jsx
@@ -1,9 +1,27 @@
 import './SignIn.css'
+import { InputText } from 'primereact/inputtext';
+import { Button } from 'primereact/button';
 
 const SignIn = () => {
     return (
         <div className="sign-in">
-            <h1>Sign In</h1>
+            <div className='si-left'>
+                <div className='sill'>
+                    <h1>Bienvenido a MindWell!</h1>
+                    <p>Inicie sesión y disfrute de los beneficios de nuestra aplicación en su vida diaria.</p>
+                </div>
+            </div>
+            <div className="si-right">
+                <div className='sir-body'>
+                    <label htmlFor="email">Email</label>
+                    <InputText id='email' placeholder='Email' />
+                    <label htmlFor="password">Contraseña</label>
+                    <InputText id='password' placeholder='Password' />
+                    <Button label='Iniciar sesión' />
+
+                    <p>¿No tienes una cuenta? <a href='/sign-up'>Registrate</a></p>
+                </div>
+            </div>
         </div>
     )
 }
--- src/pages/signup/SignUp.jsx
+++ src/pages/signup/SignUp.jsx
@@ -1,9 +1,27 @@
 import './SignUp.css'
+import { Button } from 'primereact/button';
+import { InputText } from 'primereact/inputtext';
 
 const SignUp = () => {
     return (
         <div className="sign-up">
-            <h1>Sign Up</h1>
+            <div className='su-body'>
+                <div className="sub-title">
+                    <h1>Crea tu cuenta</h1>
+                </div>
+                <p>¿Ya tienes una cuenta? <a href='/sign-in'>Inicia sesión</a></p>
+                <label htmlFor="nombre">Nombre</label>
+                <InputText id='nombre' placeholder='Nombre' />
+                <label htmlFor="edad">Edad</label>
+                <InputText id='edad' placeholder='Edad' />
+                <label htmlFor="email">Email</label>
+                <InputText id='email' placeholder='Email' />
+                <label htmlFor="contraseña">Contraseña</label>
+                <InputText id='contraseña' placeholder='Contraseña' />
+                <label htmlFor="repetir-contraseña">Repetir contraseña</label>
+                <InputText id='repetir-contraseña' placeholder='Repetir contraseña' />
+                <Button label='Registrarse' />
+            </div>
         </div>
     )
 }
